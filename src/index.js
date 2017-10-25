bdd363f5069d98a1375518e16e3e0b8aaa815151
Marces Engel <marces.engel@googlemail.com>
2017-10-25 10:16:45 +0200

    Made ImmutableFlatList a Statefull Component to allow for refs

--- src/index.js
+++ src/index.js
@@ -4,23 +4,29 @@ import {
   View
 } from 'react-native'
 
-export default ({ renderItem, data, ItemSeparatorComponent, ...props }) => (
-  <VirtualizedList
-    {...props}
-    data={data}
-    getItem={(data, index) => data.get(index)}
-    getItemCount={(data) => data.size}
-    renderItem={(itemData) => (
-      <View>
-        {
-          renderItem(itemData)
-        }
-        {
-          itemData.index < data.size - 1 && ItemSeparatorComponent
-            ? <ItemSeparatorComponent />
-            : null
-        }
-      </View>
-    )}
-  />
-)
+export default class ImmutableFlatList extends React.PureComponent {
+  render () {
+    const { renderItem, data, ItemSeparatorComponent, ...props } = this.props
+
+    return (
+      <VirtualizedList
+        {...props}
+        data={data}
+        getItem={(data, index) => data.get(index)}
+        getItemCount={(data) => data.size}
+        renderItem={(itemData) => (
+          <View>
+            {
+              renderItem(itemData)
+            }
+            {
+              itemData.index < data.size - 1 && ItemSeparatorComponent
+                ? <ItemSeparatorComponent />
+                : null
+            }
+          </View>
+        )}
+      />
+    )
+  }
+}
